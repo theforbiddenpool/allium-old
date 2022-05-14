44d9270c9b738855e0aa1c2c0a0ebe21f3533283
theforbiddenpool <contact@theforbiddenpool.com>
2022-05-14 13:18:45 +0200

    test(server): setup tests with jest

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -7,10 +7,10 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_TEST_USER,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_TEST_HOST,
     dialect: 'postgres',
   },
   production: {
