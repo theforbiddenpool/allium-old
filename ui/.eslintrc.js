01d7bb17bbb31716127565e9fd2871432b1068b9
theforbiddenpool <contact@theforbiddenpool.com>
2022-07-13 12:28:29 +0200

    chore(eslint): disable require-default-props rule

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
+    'react/require-default-props': 'off',
     'jsx-a11y/label-has-associated-control': [2, {
       assert: 'either',
     }],
