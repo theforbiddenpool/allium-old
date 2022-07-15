cb8dddd313eca4ea2f6fe6b7afbbb88ab22f92c0
theforbiddenpool <contact@theforbiddenpool.com>
2022-07-15 14:01:37 +0200

    feat(signup): add input validation to form

--- ui/components/SignUpForm/SignUpForm.tsx
+++ ui/components/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,5 @@
 import { Formik, FormikValues } from 'formik';
+import * as Yup from 'yup';
 import { Button } from '@chakra-ui/react';
 import { MdLogin } from 'react-icons/md';
 import { FormikInput } from '../layout';
@@ -7,12 +8,29 @@ interface SignUpFormProps {
   onSubmit?: (values: FormikValues) => void
 }
 
+const schema = Yup.object({
+  name: Yup.string()
+    .min(3, 'Name must contain at least 3 characters')
+    .max(250, 'Name must be 250 characters or less')
+    .required('Name is required'),
+  username: Yup.string()
+    .max(100, 'Username must be 100 characters or less')
+    .required('Username is required'),
+  email: Yup.string().email('Invalid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must contain at least 8 characters')
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 'Password is too weak')
+    .required('Password is required'),
+});
+
 function SignUpForm({ onSubmit }: SignUpFormProps) {
   return (
     <Formik
       initialValues={{
         name: '', username: '', email: '', password: '',
       }}
+      validationSchema={schema}
       onSubmit={(values, { setSubmitting }) => {
         onSubmit?.(values);
         console.log('submitted', values);
